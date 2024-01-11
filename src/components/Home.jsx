6f9d24ac88391a786dd937decd8d149e21deace6
Choudhary8055 <rahulchoudhary200599@gmail.com>
2024-01-11 14:35:30 +0530

    update the env variable

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -76,10 +76,10 @@ const Home = () => {
 
       if (errorValidator()) {
         await emailjs.send(
-          import.meta.env.VITE_EMAILJS_SERVICE_ID,
-          import.meta.env.VITE_EMAILJS_TEMPLATE_ID,
+          import.meta.env.VITE_REACT_APP_SERVICE_ID,
+          import.meta.env.VITE_REACT_APP_TEMPLATE_ID,
           templateParams,
-          import.meta.env.VITE_EMAILJS_USER_ID
+          import.meta.env.VITE_REACT_APP_USER_ID
         );
 
         // Show confirmation message
