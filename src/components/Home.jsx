9112d01a1985f963ca20d4e257551f85cbd235b5
Choudhary8055 <rahulchoudhary200599@gmail.com>
2024-01-11 14:50:13 +0530

    add console for deployment

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -75,13 +75,14 @@ const Home = () => {
       };
 
       if (errorValidator()) {
-        await emailjs.send(
+       const response = await emailjs.send(
           import.meta.env.VITE_REACT_APP_SERVICE_ID,
           import.meta.env.VITE_REACT_APP_TEMPLATE_ID,
           templateParams,
           import.meta.env.VITE_REACT_APP_USER_ID
         );
-
+     console.log("Check Email JS Response", response);
+     console.log("Check Public Key: ", import.meta.env.VITE_REACT_APP_USER_ID);
         // Show confirmation message
         setConfirmation(true);
       }
